e12fbefea32d4e09e47e15958bc46ebb43ee3516
Maksim Narushevich <narushevich.maksim@gmail.com>
2019-05-25 16:55:23 +0300

    - Updated Docker configuration for Zend app - Added README.md for instructions how to start app

--- docker/mysql/schema.sql
+++ docker/mysql/schema.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS `zend_db`;
+
+USE `zend_db`;
+
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
--- module/Portal/src/Controller/IndexController.php
+++ module/Portal/src/Controller/IndexController.php
@@ -19,16 +19,16 @@ class IndexController extends AbstractController
 {
     public function indexAction()
     {
-        try{
-            $dbh = new pdo( 'mysql:host=mysql;dbname=zend_db',
-                'docker',
-                'docker',
-                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            die(json_encode(array('outcome' => true)));
-        }
-        catch(PDOException $ex){
-            die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
-        }
+//        try{
+//            $dbh = new pdo( 'mysql:host=mysql;dbname=zend_db',
+//                'docker',
+//                'docker',
+//                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+//            die(json_encode(array('outcome' => true)));
+//        }
+//        catch(PDOException $ex){
+//            die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+//        }
 
         //########How to register new service into ServiceManager (for 1-3)
         $serviceManager=$this->getServiceManager();
